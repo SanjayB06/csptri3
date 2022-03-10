9fc456b13e3e59d3556f711642a08e8d1f6b1616
SanjayB06 <sanjaymbharadwaj@gmail.com>
2022-03-10 12:26:13 -0800

    green tree

--- week0/christmastree.py
+++ week0/christmastree.py
@@ -3,7 +3,7 @@ def printTree():
     for x in range(row):
         x+=1
         spaces = row-x
-        print(" "*spaces,"* "*x)
+        print(" "*spaces,"\033[0;32m*\033[00m "*x)
     print(" "*(row-3),"* "*3)
     print(" "*(row-3),"* "*3)
 if __name__ == "__main__":
