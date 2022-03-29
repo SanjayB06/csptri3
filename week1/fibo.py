c6fdd867c26d0e36033586e7ab0991318a4b557b
simon <simoncbrunzell@gmail.com>
2022-03-29 14:01:00 -0700

    clearing away errors, and cleaning up code

--- week1/fibo.py
+++ week1/fibo.py
@@ -1,12 +1,15 @@
 
 def fibo(n):
-    if n == 1 or n==0:
+    if n == 1 or n == 0:
         return n 
     return fibo(n-1) + fibo(n-2)
 
-def printFibo():
+
+def print_fibo():
     n = int(input("Enter a number: "))
     output = [fibo(x+1) for x in range(n)]
     print(*output)
+
+
 if __name__ == "__main__":
-    printFibo()
+    print_fibo()
--- week1/hack.py
+++ week1/hack.py
@@ -1,29 +1,30 @@
 # Hack 1: 
 
-#InfoDb contains dictionaries that each have data about movies
+# InfoDb contains dictionaries that each have data about movies
+
+
 InfoDb = []
 InfoDb.append({  
                "Title": "Jurassic Park",  
                "Year": "1993",  
                "Director": "Steven Spielberg",  
-               "Genres":["Sci-fi","Action"]  
+               "Genres": ["Sci-fi", "Action"]
               })  
 InfoDb.append({  
                "Title": "The Dark Knight",  
                "Year": "2008",  
                "Director": "Christopher Nolan",  
-               "Genres":["Action","Crime","Thriller"]  
+               "Genres": ["Action", "Crime", "Thriller"]
               })  
 InfoDb.append({  
                "Title": "The Godfather",  
                "Year": "1972",  
                "Director": "Francis Ford Coppola",  
-               "Genres":["Crime","Drama"]  
+               "Genres": ["Crime", "Drama"]
               })  
 InfoDb.append({  
                "Title": "The Batman",  
                "Year": "2022",  
                "Director": "Matt Reeves",  
-               "Genres":["Crime","Superhero","Action"]  
+               "Genres": ["Crime", "Superhero", "Action"]
               })  
-
