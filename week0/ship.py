ced97b853676e9db173fd16e969d59d0c416058a
simon <simoncbrunzell@gmail.com>
2022-03-29 13:57:52 -0700

    granted users some insight into the most advantages inputs for ship.py

--- week0/ship.py
+++ week0/ship.py
@@ -1,14 +1,21 @@
 import os
 import time
-def printShip():
-    distance = int(input("How far should the ship go? "))
+
+
+def print_ship():
+    print("Honestly anything less than 50 is like nothing ")
+    print("And more than 100 is like past the screen")
+    distance = int(input("So, how far should the ship go? "))
     for x in range(int(distance)):
-        print("  "*x,"    |\ ")
-        print("  "*x,"    |/ ")
-        print("  "*x,"\__ |__/ ")
-        print("  "*x," \____/ ")
-        print("\u001b[34m {dashes} \u001b[37m".format(dashes = "--"*distance))
-        os.system("clear")
+        print("  "*x, "    |\ ")
+        print("  "*x, "    |/ ")
+        print("  "*x, "\__ |__/ ")
+        print("  "*x, " \____/ ")
+        print("\u001b[34m {dashes} \u001b[37m".format(dashes="--"*distance))
+
+        # os.system("clear")
         time.sleep(0.006)
+
+
 if __name__ == "__main__":
-    printShip()
+    print_ship()
