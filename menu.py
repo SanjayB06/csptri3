432e6ed752e749bbde14d39945bd970b54319504
simon <simoncbrunzell@gmail.com>
2022-03-29 14:19:54 -0700

    clearing away errors, and cleaning up code

--- menu.py
+++ menu.py
@@ -1,25 +1,29 @@
 from menulist import mainMenu
+
+
 def buildMenu(menu):
-    for key,value in menu.items(): 
+    for key, value in menu.items():
         display = value["display"]
-        print(f"{key} ------ {display}") # each menu item is printed
-    print("What is your choice? (enter the number value) ") # user input promp
+        print(f"{key} ------ {display}")  # each menu item is printed
+    print("What is your choice? (enter the number value) ")  # user input promp
+
 
 def presentMenu(menu):
-    buildMenu(menu) #print out menu and take input
+    buildMenu(menu)  # print out menu and take input
     choice = int(input())
-    while choice not in menu: # ensure that choice is valid
+    while choice not in menu:  # ensure that choice is valid
         choice = int(input("Please elect a valid item. "))
-    if (choice) in menu:
-        if menu[choice]["type"] == "func": #determine whether recursion is needed
-            menu[choice]["exec"]() #run function
+    if choice in menu:
+        if menu[choice]["type"] == "func":  # determine whether recursion is needed
+            menu[choice]["exec"]()  # run function
 
         else:
-            presentMenu(menu[choice]["exec"]) #display submenu
+            presentMenu(menu[choice]["exec"])  # display submenu
+
 
 if __name__ == "__main__":
-    while True: #forever loop
+    while True:  # forever loop
         presentMenu(mainMenu)
-        halt = input("Do you want to continue (y/n)? ") #checks if user wants to go again
+        halt = input("Do you want to continue (y/n)? ")  # checks if user wants to go again
         if halt.lower() == "n":
             break
--- menulist.py
+++ menulist.py
@@ -2,74 +2,79 @@ from week0.christmastree import printTree
 from week0.ship import printShip
 from tri2.keypad import print_matr
 from tri2.swap import swap
-from week1.fibo import fibo,printFibo
+from week1.fibo import fibo, printFibo
 from week1.hack import InfoDb
-from week1.hacktwo import for_loop,while_loop,recursive_loop
+from week1.hacktwo import for_loop, while_loop, recursive_loop
 from week2.hack2 import dispfac
 from week2.hack2 import dispSeries
 from week2.hack3 import superfac
 from week2.hack4 import printpal
+
+
 def f1():
     print('f1')
+
+
 def f2():
     print('f2')
 
+
 subMenu = {
-    1: {"display":"f1",
-    "exec":f1,
-    "type":"func"},
-    2: {"display":"f2",
-    "type":"func",
-    "exec":f2,},
+    1: {"display": "f1",
+    "exec": f1,
+    "type": "func"},
+    2: {"display": "f2",
+    "type": "func",
+    "exec": f2, },
     3: {
         "display": "Quit program",
-        "exec":quit,
-        "type":"func"
+        "exec": quit,
+        "type": "func"
     }
 
 }
 week1hack2Menu = {
     1: {
-        "display":"Hack 2a (for loop)",
+        "display": "Hack 2a (for loop)",
         "exec": for_loop,
-        "type":"func"
+        "type": "func"
     },
     2: {
-        "display":"Hack 2b (while loop)",
+        "display": "Hack 2b (while loop)",
         "exec": while_loop,
-        "type":"func"
+        "type": "func"
     },
     3: {
-        "display":"Hack 2c (recursive)",
+        "display": "Hack 2c (recursive)",
         "exec": recursive_loop,
-        "type":"func"
+        "type": "func"
     },
     4: {
-        "display":"Quit program",
+        "display": "Quit program",
         "exec": quit,
-        "type":"func"
+        "type": "func"
     },
 }
 drawingMenu = {
     1: {
-        "display":"Christmas Tree",
-        "exec":printTree,
-        "type":"func"
+        "display": "Christmas Tree",
+        "exec": printTree,
+        "type": "func"
     },
     2: {
-        "display":"Print Ship",
-        "exec":printShip,
-        "type":"func"
+        "display": "Print Ship",
+        "exec": printShip,
+        "type": "func"
     },
     3: {
-        "display":"Keypad",
-        "exec":print_matr,
-        "type":"func"
+        "display": "Keypad",
+        "exec": print_matr,
+        "type": "func"
     },
     4: {
         "display": "quit",
-        "exec":quit,
-        "type":"func"
+        "exec": quit,
+        "type": "func"
     }
 }
 factorialMenu = {
@@ -96,19 +101,19 @@ factorialMenu = {
 }
 mathMenu = {
     1: {
-        "display":"Fibonacci",
-        "exec":printFibo,
-        "type":"func"
+        "display": "Fibonacci",
+        "exec": printFibo,
+        "type": "func"
     },
     2: {
-        "display":"Factorials",
-        "exec":factorialMenu,
-        "type":"submenu"
+        "display": "Factorials",
+        "exec": factorialMenu,
+        "type": "submenu"
     },
     3: {
-        "display":"quit",
-        "exec":quit,
-        "type":"func"
+        "display": "quit",
+        "exec": quit,
+        "type": "func"
     }
 }
 
@@ -118,32 +123,32 @@ mainMenu = {
         "exec": drawingMenu,
         "type": "submenu"
     },
-    2: {"display":"Swap ",
-    "exec":swap,
-    "type":"func"},
+    2: {"display": "Swap ",
+    "exec": swap,
+    "type": "func"},
     3: {
-        "display":"Submenu",
+        "display": "Submenu",
         "exec": subMenu,
-        "type":"submenu"
+        "type": "submenu"
     },
     4: {
-        "display":"Week 1 Hack 2 (InfoDb)",
+        "display": "Week 1 Hack 2 (InfoDb)",
         "exec": week1hack2Menu,
-        "type":"submenu"
+        "type": "submenu"
     },
     5: {
-        "display":"Math Functions",
+        "display": "Math Functions",
         "exec": mathMenu,
-        "type":"submenu"
+        "type": "submenu"
     },
     6: {
-        "display":"Palindrome",
+        "display": "Palindrome",
         "exec": printpal,
-        "type":"func"
+        "type": "func"
     },
     7: {
         "display": "Quit program",
-        "exec":quit,
-        "type":"func"
+        "exec": quit,
+        "type": "func"
     }
 }
