c5effcf2cdf04aea04071e1fdbdf2112ee224494
simon <simoncbrunzell@gmail.com>
2022-03-29 13:47:12 -0700

    Cleaning up some Python Code

---
+++ Cross_Over/Affirmations.py
@@ -0,0 +1,32 @@
+
+# Making it personal
+user = input("what's your name: ")
+x = int(input("now pick a number 1-5: "))
+
+
+# Spams Compliments
+while x == 1:
+    print(user + " is great")
+
+while x == 2:
+    print(user + " is sooo smart")
+
+while x == 3:
+    print(user + " is such a hard worker")
+
+while x == 4:
+    print(user + " is legendary")
+
+while x == 5:
+    print(user + "no one codes like " + user)
+    print()
+    print()
+    print()
+    print(user + "no thinks like  " + user)
+    print()
+    print()
+    print()
+    print(user + "no one's deployment plan is as great as  " + user)
+    print()
+    print()
+    print()
