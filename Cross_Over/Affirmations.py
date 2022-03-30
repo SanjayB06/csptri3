6a8f83289b9cafeaed01423489d7375aabeca6ed
simon <simoncbrunzell@gmail.com>
2022-03-30 13:03:49 -0700

    created a fun affirmation program

--- Cross_Over/Affirmations.py
+++ Cross_Over/Affirmations.py
@@ -1,32 +1,78 @@
+# Spams Compliments
+def speak():
+    b = 0
+    while compliment == 1:
+        print(user + " is great")
+        b = b + 1
+        if b >= 250000:
+            options()
+            ask()
+            break
+    while compliment == 2:
+        print(user + " is sooo smart")
+        b = b + 1
+        if b >= 250000:
+            options()
+            ask()
+            break
+    while compliment == 3:
+        print(user + " is such a hard worker")
+        b = b + 1
+        if b >= 250000:
+            options()
+            ask()
+            break
+    while compliment == 13:
+        print(user + " no one codes like " + user)
+        print()
+        print()
+        print()
+        print(user + " no thinks like  " + user)
+        print()
+        print()
+        print()
+        print(user + " no one's deployment plan is as great as  " + user)
+        print()
+        print()
+        print()
+        b = b + 1
+        if b >= 50000:
+            options()
+            ask()
+            break
+
+
+# saved items
+Options = ["1 - great", "2 - smart", "3 - hard-worker", "13 - ?????"]
+Border = "----------------"
+compliment = "un-picked"
+
+
+def options():
+    # Present Options
+    print()
+    print(Border)
+    for x in Options:
+        print(x)
+    print(Border)
+    print()
+
+
+options()
 
 # Making it personal
-user = input("what's your name: ")
-x = int(input("now pick a number 1-5: "))
+user = input("Hiya what's your name: ")
 
 
-# Spams Compliments
-while x == 1:
-    print(user + " is great")
+# made it a function so I can re-ask
+def ask():
+    print()
+    global compliment
+    compliment = int(input("now which option do you want?: "))
+    if compliment != 1 or 2 or 3 or 13:
+        print("Please pick a proper option")
+    speak()
 
-while x == 2:
-    print(user + " is sooo smart")
 
-while x == 3:
-    print(user + " is such a hard worker")
-
-while x == 4:
-    print(user + " is legendary")
-
-while x == 5:
-    print(user + "no one codes like " + user)
-    print()
-    print()
-    print()
-    print(user + "no thinks like  " + user)
-    print()
-    print()
-    print()
-    print(user + "no one's deployment plan is as great as  " + user)
-    print()
-    print()
-    print()
+# asking which one they want
+ask()
